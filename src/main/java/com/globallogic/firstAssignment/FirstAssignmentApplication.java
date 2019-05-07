40c9a072b751421d7d1ca841d0ed5811928b4e5b
yogesh paisode <yogesh@gg.com>
2019-05-07 19:32:31 +0530

    first

---
+++ src/main/java/com/globallogic/firstAssignment/FirstAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.globallogic.firstAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAssignmentApplication.class, args);
+	}
+
+}
