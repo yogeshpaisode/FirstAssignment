97b113cc9761145fb5c4910f46338c7f651c82c3
yogesh paisode <yogesh@gg.com>
2019-05-20 18:41:24 +0530

    JAR ==> War

--- src/main/java/com/globallogic/firstAssignment/FirstAssignmentApplication.java
+++ src/main/java/com/globallogic/firstAssignment/FirstAssignmentApplication.java
@@ -3,6 +3,7 @@ package com.globallogic.firstAssignment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -11,7 +12,7 @@ import org.springframework.context.annotation.Import;
 
 @Import(SwaggerConfig.class)
 @EnableAutoConfiguration
-public class FirstAssignmentApplication {
+public class FirstAssignmentApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstAssignmentApplication.class, args);
