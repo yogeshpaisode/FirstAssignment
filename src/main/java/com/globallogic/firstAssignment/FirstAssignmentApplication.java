1725756d9293fb970e57fa5235bd3e1933ed7e0a
yogesh paisode <yogesh@gg.com>
2019-05-20 11:32:19 +0530

    Added swagger.

--- src/main/java/com/globallogic/firstAssignment/FirstAssignmentApplication.java
+++ src/main/java/com/globallogic/firstAssignment/FirstAssignmentApplication.java
@@ -1,9 +1,16 @@
 package com.globallogic.firstAssignment;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Configuration
+
+@Import(SwaggerConfig.class)
+@EnableAutoConfiguration
 public class FirstAssignmentApplication {
 
 	public static void main(String[] args) {
