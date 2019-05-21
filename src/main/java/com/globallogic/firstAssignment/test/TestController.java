827d87f227ae65f31cfefcd6823965d3f972b1c7
yogesh paisode <yogesh@gg.com>
2019-05-21 12:18:19 +0530

    Test

---
+++ src/main/java/com/globallogic/firstAssignment/test/TestController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.globallogic.firstAssignment.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author yogesh.paisode
+ */
+@Controller
+@RequestMapping(path = "/test")
+public class TestController {
+    
+    @GetMapping("/testapi")
+    public String getTestApi() {
+        return "ok";
+    }
+    
+}
