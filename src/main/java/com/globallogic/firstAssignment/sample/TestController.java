cf8660583748f0f994faaf4734c77f2fb91bdaa4
yogesh paisode <yogesh@gg.com>
2019-05-21 16:08:43 +0530

    ssdads

--- src/main/java/com/globallogic/firstAssignment/sample/TestController.java
+++ src/main/java/com/globallogic/firstAssignment/sample/TestController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.globallogic.firstAssignment.test;
+package com.globallogic.firstAssignment.sample;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
